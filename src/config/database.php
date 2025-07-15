debf9d3e0b3b342cfe0b65ed019100a9bf5f6128
Bryan Caudel <bryancaudelh@gmail.com>
2025-07-14 22:45:41 -0600

    Docker

--- src/config/database.php
+++ src/config/database.php
@@ -1,10 +1,19 @@
 <?php 
-$conexion= mysqli_connect('localhost',"root","",'u422899116_evaluacion360');
-if(!$conexion){
-    echo"Conexion fallida";
+$host = 'db';
+$user = 'root';
+$password = ''; // sin contraseña
+$database = 'u422899116_evaluacion360';
+
+$intentos = 10;
+while ($intentos > 0) {
+    $conexion = @mysqli_connect($host, $user, $password, $database);
+    if ($conexion) break;
+    echo "Esperando conexión con la base de datos...<br>";
+    sleep(2);
+    $intentos--;
+}
+
+if (!$conexion) {
+    die("Conexión fallida: " . mysqli_connect_error());
 }
 ?>
-
-
-
-
